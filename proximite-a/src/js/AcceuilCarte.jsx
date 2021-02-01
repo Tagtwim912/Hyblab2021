9dbd7f111a0977dada281decc73b058fb7fd3211
E17C549Q <tristan.desgranges@etu.univ-nantes.fr>
2021-02-01 12:56:10 +0100

    remerge dev into FrontPagesTheme

--- proximite-a/src/js/AcceuilCarte.jsx
+++ proximite-a/src/js/AcceuilCarte.jsx
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
-import {MapContainer, TileLayer, Marker, Popup, Circle} from 'react-leaflet'
+import {MapContainer, TileLayer, Marker, Popup, Polygon, Polyline} from 'react-leaflet'
 import '../css/AcceuilCarte.css'
 import CarteInterractionChoixLieu from './CarteInterractionChoixLieu'
 import PopupAnnonce from './PopupAnnonce'
@@ -11,7 +10,7 @@ import {getPosition} from "leaflet/src/dom/DomUtil";
 import equivalent from './equivalent.js'
 const decallageCentrageCarte = 0.004;
 const decallageMarqueur = 0.0005;
-
+const redOptions = { color: '#00d66f' }
 
 function GetIcon(type, _iconsize, theme){
     switch (type) {
@@ -28,127 +27,12 @@ function GetIcon(type, _iconsize, theme){
                 iconUrl : theme.default,
                 iconSize: [_iconsize, 39],
                 iconAnchor:[_iconsize/2,39],
-                popupAnchor:[_iconsize/2,39]
-            });
-            break
-    }
-}
-
-
-
-class AcceuilCarte extends  React.Component {
-    state={
-        popupPhase : 2,
-        currentPosition: this.props.data.coords,
-        sites:this.props.data.sites,
-        adresse:this.props.data.adresse,
-        moyenId:this.props.data.moyenId,
-        nomPers:this.props.data.nomPers,
-    };
-
-
-
-
-    getPhase(nom){
-        switch (this.state.popupPhase) {
-            case 0:
-                return null;
-            case 1:
-                return <CarteInterractionChoixLieu data={this.state} nomPers={nom.nomPers} onNextPhase={this.nextPhase}/>;
-            case 2:
-                return <CarteInterractionChoixMultiples data={this.state} nomPers={nom.nomPers} onSetMoyen={this.updateMoyen} onNextPhase={this.nextPhase}/>;
-            case 3:
-                return <CarteInterractionChoixMultiplesReduit data={this.state} nomPers={nom.nomPers} onSetMoyen={this.updateMoyen} onPreviousPhase={this.previousPhase}/>;
-        }
-    };
-
-    nextPhase = () =>{
-        const newPopupPhase = this.state.popupPhase+1;
-        this.setState({popupPhase:newPopupPhase })
-    };
-    previousPhase = () =>{
-        const newPopupPhase = this.state.popupPhase-1;
-        this.setState({popupPhase:newPopupPhase })
-    };
-
-    updateMoyen = (e) => {
-        this.setState({ moyenId: e })
-        this.props.onSetMoyen(e)
-    };
-
-    generateItineraire = (dest) => {
-        console.log(`demande de tracage d'itineraire de ${this.state.currentPosition} vers ${dest}`)
-
-    }
-
-
-    render() {
-        const {nomPers} = this.props;
-        const redOptions = { color: '#999999' }
-        return (
-            <div id="map">
-                <MapContainer center={[this.state.currentPosition[0],this.state.currentPosition[1]-decallageCentrageCarte]} zoom={16} scrollWheelZoom={true}>
-                    <TileLayer url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"} />
-                    <Marker icon={GetIcon(1,30)}  position={[this.state.currentPosition[0]+decallageMarqueur,this.state.currentPosition[1]]}></Marker>
-                    {this.state.sites.map( (e) => {
-                        return <Marker icon={GetIcon(2,20, equivalent.themePicto.get(e.type))}  position={[e.coordonnes[0]+decallageMarqueur,(e.coordonnes[1])]}>
-                                <Popup> A pretty CSS3 popup. <br />
-                                    Easily customizable.<hr/>
-                                    {console.log(e)}
-                                    <input type="button" class="btn btn-primary" value="S'y rendre" onClick={ ()=>{this.generateItineraire(e.coordonnes)} }/>
-                                </Popup>
-                        </Marker>
-                    }) }
-                    <Circle center={this.state.currentPosition} pathOptions={redOptions} radius={500} />
-                </MapContainer>
-
-                <PopupAnnonce/>
-                {this.getPhase({nomPers})}
-                <div id="containerButtonsMapNavigation">
-                    <a href="https://www.google.com/" class="buttonMapNavigation">Ouvrir l’itinéraire avec GoogleMaps</a>
-                    <input type="button" class="buttonMapNavigation" value="Télécharger la carte en PDF"/>
-                    <input type="button" class="buttonMapNavigation" value="Crédits"/>
-                </div>
-            </div>
-        );
-    }
-}
-export default AcceuilCarte;
-import React from 'react';
-import {MapContainer, TileLayer, Marker, Popup, Polygon} from 'react-leaflet'
-import '../css/AcceuilCarte.css'
-import CarteInterractionChoixLieu from './CarteInterractionChoixLieu'
-import PopupAnnonce from './PopupAnnonce'
-import CarteInterractionChoixMultiples from './CarteInterractionChoixMultiples'
-import CarteInterractionChoixMultiplesReduit from './CarteInterractionChoixMultiplesReduit'
-import L from "leaflet"
-import {getPosition} from "leaflet/src/dom/DomUtil";
-import equivalent from './equivalent.js'
-import { Polyline } from 'leaflet';
-const decallageCentrageCarte = 0.004;
-const decallageMarqueur = 0.0005;
-
-
-function GetIcon(type, _iconsize, theme){
-    switch (type) {
-        case 1: //position actuelle
-            return L.icon({
-                iconUrl : require("../img/pictogrammes_maison.png").default,
-                iconSize: [_iconsize, 39],
-                iconAnchor:[_iconsize/2,39],
                 popupAnchor:[0,-39]
             });
             break
-        case 2: //site
-            return L.icon({
-                iconUrl : theme.default,
-                iconSize: [_iconsize, 39],
-                iconAnchor:[_iconsize/2,39],
-                popupAnchor:[_iconsize/2,39]
-            });
-            break
     }
 }
+
 
 
 
@@ -160,7 +44,7 @@ class AcceuilCarte extends  React.Component {
         adresse:this.props.data.adresse,
         moyenId:this.props.data.moyenId,
         nomPers:this.props.data.nomPers,
-        perimetre: [],
+        perimetre: this.props.data.perimetre,
         itineraire: [],
     };
 
@@ -195,39 +79,52 @@ class AcceuilCarte extends  React.Component {
     };
 
     generateItineraire = (dest) => {
-        fetch(`http://localhost:8080/proximite-a/api/getItinerary/${this.state.moyenId}/${this.props.data.coords}/${dest}`)
+        let moyenTransport = ['foot-walking', 'foot-walking', 'cycling-regular', 'wheelchair', 'cycling-road', 'cycling-regular', 'cycling-regular'][this.state.moyenId];
+        fetch(`http://localhost:8080/proximite-a/api/getItinerary/${moyenTransport}/${[this.props.data.coords[1],this.props.data.coords[0]]}/${[dest[1],dest[0]]}`)
+        .then(itineraire=> itineraire.json())
         .then(itineraire => {
-            this.setState({itineraire});
+            console.log(itineraire)
+            let newA=[];
+            itineraire.forEach((l) => {
+                newA.push([l[1],l[0]])
+            });
+            this.setState({itineraire:newA});
         })
+    };
+
+    getPolyne = () => {
+        if(typeof this.state.itineraire !== 'undefined' && this.state.itineraire.length > 0){
+            return <Polyline positions={[this.state.itineraire]} pathOptions={redOptions}/>
+        }
     }
 
-    generatePerimetre = () => {
-        fetch(`http://localhost:8080/proximite-a/api/get15minzone/${this.state.moyenId}/${this.state.currentPosition}/`)
-        .then(perimetre => {
-            this.setState({perimetre});
-        })
+    getPolygone = () => {
+        if(typeof this.state.perimetre !== 'undefined' && this.state.perimetre.length > 0){
+            return <Polygon positions={this.state.perimetre} pathOptions={redOptions} />
+        }
     }
 
     render() {
-        this.generatePerimetre();
+        console.log("render Acceil")
+        console.log(this.state)
         const {nomPers} = this.props;
-        const redOptions = { color: '#999999' }
         return (
             <div id="map">
                 <MapContainer center={[this.state.currentPosition[0],this.state.currentPosition[1]-decallageCentrageCarte]} zoom={16} scrollWheelZoom={true}>
                     <TileLayer url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"} />
                     <Marker icon={GetIcon(1,30)}  position={[this.state.currentPosition[0]+decallageMarqueur,this.state.currentPosition[1]]}></Marker>
                     {this.state.sites.map( (e) => {
-                        return <Marker icon={GetIcon(2,20, equivalent.themePicto.get(e.type))}  position={[e.coordonnes[0]+decallageMarqueur,(e.coordonnes[1])]}>
-                                <Popup> A pretty CSS3 popup. <br />
-                                    Easily customizable.<hr/>
-                                    {console.log(e)}
-                                    <input type="button" class="btn btn-primary" value="S'y rendre" onClick={ ()=>{this.generateItineraire(e.coordonnes)} }/>
-                                </Popup>
+                        return <Marker icon={GetIcon(2,30, equivalent.themePicto.get(e.type))}  position={[e.coordonnes[0]+decallageMarqueur,(e.coordonnes[1])]}>
+                            <Popup>
+                                <b>{e.titre}</b>
+                                <hr/>
+                                <input type="button" class="btn btn-primary" value="S'y rendre" onClick={ ()=>{this.generateItineraire(e.coordonnes)} }/>
+                            </Popup>
                         </Marker>
                     }) }
                     <Polygon positions={this.state.perimetre} pathOptions={redOptions} />
-                    <Polyline positions={this.state.itineraire}/>
+                    {this.getPolygone()}
+                    {this.getPolyne()}
                 </MapContainer>
 
                 <PopupAnnonce/>
@@ -240,5 +137,8 @@ class AcceuilCarte extends  React.Component {
             </div>
         );
     }
+
+
+
 }
 export default AcceuilCarte;
--- proximite-a/src/js/App.js
+++ proximite-a/src/js/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Home from './Home';
 import Theme from './Theme';
@@ -15,201 +14,7 @@ class App extends  React.Component {
         themeId:0,
         moyenId:0,
         coords:[47.215878,-1.55051],
-        adresse: {
-            rue: '',
-            codepostal: '',
-            ville: ''
-        },
-        sites: [{ id: '1',
-                titre: 'squalala',
-                img: 'https://pbs.twimg.com/profile_images/743774363833503744/-eSLwh6f_400x400.jpg',
-                adresse: 'nous sommes partis',
-                description : "blabla",
-                coordonnes:[1,2],    //latitude_longitude
-                type:4,
-             },
-            { id: '2',
-                titre: 'squelele',
-                img: 'https://media.tenor.com/images/1c500d0d30c039aef2cc1056a0f4e8e6/tenor.gif',
-                adresse: 'nous sommes repartis',
-                description : "blabla",
-                coordonnes:[3,4],    //latitude_longitude
-                type:1,
-            },
-            { id: '3',
-                titre: 'Oooo di',
-                img:'https://media.tenor.com/images/5c58bbed210c8bb91dddb88caa8f1ed3/tenor.gif',
-                adresse:'AAAAAAAAAAAAAHH',
-                description : "blabla",
-                coordonnes:[5,6],    //latitude_longitude
-                type:2,
-            }],
-            surprise: {
-                titre: 'Oooo di',
-                img: 'https://media.tenor.com/images/5c58bbed210c8bb91dddb88caa8f1ed3/tenor.gif',
-                adresse: 'AAAAAAAAAAAAAHH',
-                description: "blabla",
-                coordonnes: [5, 6],    //latitude_longitude
-                type: 3
-        }
-    };
-
-    getPage = () => {
-        switch (this.state.pageId) {
-            case 0:
-                return <Home onNextPage={this.nextPage} updateNom={this.updateName}/>;
-            case 1:
-                return <Theme data={this.state} onNextPage={this.nextPage} onSetTheme={this.updateTheme} onPreviousPage={this.previousPage}/>;
-            case 2:
-                return <Attributs data={this.state} onNextPage={this.nextPage} onSetAttributs={this.updateAttributs} onPreviousPage={this.previousPage}/>;
-            case 3:
-                return <Moyen data={this.state} onSetMoyen={this.updateMoyen} onPreviousPage={this.previousPage} />;
-            case 4:
-                return <AcceuilCarte data={this.state} onSetMoyen={this.updateMoyen} nomPers={this.state.nomPers}/>;
-        }
-    };
-
-
-
-
-    nextPage = () =>{
-        const newPageId = this.state.pageId+1;
-        this.setState({pageId:newPageId })
-    };
-    previousPage = () =>{
-        const newPageId = this.state.pageId-1;
-        this.setState({pageId:newPageId })
-    };
-
-    updateName=(e)=>{
-        this.setState({nomPers:e})
-    };
-
-    updateTheme = (e) => {
-        this.setState({ themeId: e })
-    };
-
-    updateAttributs = (e,f) => {
-        this.setState({ coords: e,adresse:f})
-    };
-
-    updateMoyen=(e)=>{
-        this.setState({moyenId:e})
-        this.createSites()
-    };
-
-    //changer url
-    createSites = async function() {
-        let stringAdresse = this.state.adresse.rue.replace(' ', '+') + '+' + this.state.adresse.codepostal + '+' + this.state.adresse.ville
-        let moyen=equivalent.moyenEquiv.get(this.state.moyenId)
-        let theme=equivalent.themeEquiv.get(this.state.themeId)
-        console.log("appel de "+'http://localhost:8080/proximite-a/api/getlocationsforprofile/' + stringAdresse+'/'+moyen+'/'+theme);
-        let lieux = await( await fetch('http://localhost:8080/proximite-a/api/getlocationsforprofile/' + stringAdresse+'/'+moyen+'/'+theme)).json();
-        let newSites=[]
-        console.log(lieux)
-        console.log(lieux.lieux[0])
-        console.log("appel de " + `http://localhost:8080/proximite-a/api/coordinates/${lieux.lieux[0].lat}_${lieux.lieux[0].lon}`);
-        if(lieux.lieux.lenght>=1){
-            let adresse1 = await (await fetch(`http://localhost:8080/proximite-a/api/coordinates/${lieux.lieux[0].lat}_${lieux.lieux[0].lon}`)).json();
-            let name1 = ""
-            if (lieux.lieux[0].tags.name) {
-                name1 = lieux.lieux[0].tags.name
-            }
-            let site1 = {
-                id: '1',
-                titre: name1,
-                img: '',
-                adresse: adresse1,
-                description: '',
-                coordonnes: [lieux.lieux[0].lat, lieux.lieux[0].lon],
-                type: this.state.themeId
-            };
-            newSites.push(site1)
-    }
-        if (lieux.lieux.lenght>=2){let adresse2 = await (await fetch(`http://localhost:8080/proximite-a/api/coordinates/${lieux.lieux[1].lat}_${lieux.lieux[1].lon}`)).json();
-            let name2 = ""
-            if (lieux.lieux[1].tags.name) {
-                name2 = lieux.lieux[1].tags.name
-            }
-            let site2 = {
-                id: '2',
-                titre: name2,
-                img: '',
-                adresse: adresse2,
-                description: '',
-                coordonnes: [lieux.lieux[1].lat, lieux.lieux[1].lon],
-                type: this.state.themeId
-            }
-            newSites.push(site2)
-    }
-        if (lieux.lieux.lenght >= 3) {let adresse3 = await (await fetch(`http://localhost:8080/proximite-a/api/coordinates/${lieux.lieux[2].lat}_${lieux.lieux[2].lon}`)).json();
-            let name3 = ""
-            if (lieux.lieux[2].tags.name) {
-                name3 = lieux.lieux[2].tags.name
-            }
-            let site3 = {
-                id: '3',
-                titre: name3,
-                img: '',
-                adresse: adresse3,
-                description: '',
-                coordonnes: [lieux.lieux[2].lat, lieux.lieux[2].lon],
-                type: this.state.themeId
-            }
-            newSites.push(site3)
-    }
-        let lieuSurprise = this.state.surprise
-        if (lieux.surprise.lenght>=1){let adresseSurp = await (await fetch(`http://localhost:8080/proximite-a/api/coordinates/${lieux.surprise.lat}_${lieux.surprise.lon}`)).json();
-            console.log(adresseSurp)
-            let nameSurp = ""
-            if (lieux.surprise.tags.name) {
-                nameSurp = lieux.surprise.tags.name
-            }
-            let lieuSurprise = {
-                titre: nameSurp,
-                img: '',
-                adresse: adresseSurp,
-                description: '',
-                coordonnes: [lieux.surprise.lat, lieux.surprise.lon],
-                type: 0 //todo mettre le bon theme
-            }
-        }
-
-        this.setState({
-            sites: newSites,
-            surprise:lieuSurprise
-        });
-        console.log("nouveaux sites")
-        console.log(newSites)
-        this.setState({pageId:4})
-    }
-
-    render() {
-        return (
-            <div id="mainContainer">
-                {this.getPage()}
-            </div>
-        );
-    }
-}
-
-export default App;
-import React from 'react';
-import Home from './Home';
-import Theme from './Theme';
-import Attributs from './Attributs';
-import Moyen from './Moyen';
-import AcceuilCarte from './AcceuilCarte';
-import equivalent from './equivalent.js'
-
-class App extends  React.Component {
-
-    state = {
-        nomPers: null,
-        pageId: 1,
-        themeId:0,
-        moyenId:0,
-        coords:[47.215878,-1.55051],
+        perimetre: [],
         adresse: {
             rue: '',
             codepostal: '',
@@ -265,8 +70,6 @@ class App extends  React.Component {
     };
 
 
-
-
     nextPage = () =>{
         const newPageId = this.state.pageId+1;
         this.setState({pageId:newPageId })
@@ -289,20 +92,39 @@ class App extends  React.Component {
     };
 
     updateMoyen=(e)=>{
-        this.setState({moyenId:e})
-        this.createSites()
+        this.setState({moyenId:e});
+
+        this.generatePerimetre();
     };
 
+    generatePerimetre = () => {
+        let moyenTransport = ['foot-walking', 'foot-walking', 'cycling-regular', 'wheelchair', 'cycling-road', 'cycling-regular', 'cycling-regular'][this.state.moyenId];
+        fetch(`http://localhost:8080/proximite-a/api/get15minzone/${this.state.coords[1]}_${this.state.coords[0]}/${moyenTransport}`)
+            .then(perimetre=> perimetre.json())
+            .then(perimetre => {
+                console.log(perimetre)
+                let abc=[];
+                perimetre[0].forEach((l) => {
+                    console.log(l)
+                    abc.push([l[1],l[0]])
+                });
+                console.log(abc)
+                this.setState({perimetre:abc});
+                this.createSites()
+            })
+    };
+
+
+
     //changer url
-    createSites = async function() {
-        let stringAdresse = this.state.adresse.rue.replace(' ', '+') + '+' + this.state.adresse.codepostal + '+' + this.state.adresse.ville
-        let moyen=equivalent.moyenEquiv.get(this.state.moyenId)
-        let theme=equivalent.themeEquiv.get(this.state.themeId)
-        console.log("appel de "+'http://localhost:8080/proximite-a/api/getlocationsforprofile/' + stringAdresse+'/'+moyen+'/'+theme);
-        let lieux = await( await fetch('http://localhost:8080/proximite-a/api/getlocationsforprofile/' + stringAdresse+'/'+moyen+'/'+theme)).json();
-        console.log(lieux)
-        console.log(lieux.lieux.lenght)
-        console.log("appel de " + `http://localhost:8080/proximite-a/api/coordinates/`);
+    //changer url
+    createSites = async function () {
+        let stringAdresse = this.state.adresse.rue.split(' ').join('+') + '+' + this.state.adresse.codepostal.split(' ').join('+') + '+' + this.state.adresse.ville.split(' ').join('+')
+        let moyen = equivalent.moyenEquiv.get(this.state.moyenId)
+        let theme = equivalent.themeEquiv.get(this.state.themeId)
+        console.log("appel de " + 'http://localhost:8080/proximite-a/api/getlocationsforprofile/' + stringAdresse + '/' + moyen + '/' + theme);
+        let lieux = await (await fetch('http://localhost:8080/proximite-a/api/getlocationsforprofile/' + stringAdresse + '/' + moyen + '/' + theme)).json();
+
         let site1 = {
             id: '1',
             titre: '',
@@ -312,8 +134,9 @@ class App extends  React.Component {
             coordonnes: [1, 2],    //latitude_longitude
             type: 4,
         }
-        if(lieux.lieux.lenght>=1&&lieux.lenght>=1){
+        if (lieux.lieux.length >= 1 && typeof lieux.lieux !== "undefined") {
             let adresse1 = await (await fetch(`http://localhost:8080/proximite-a/api/coordinates/${lieux.lieux[0].lat}_${lieux.lieux[0].lon}`)).json();
+            let adresseF1 = adresse1.rue + " " + adresse1.codepostal + " " + adresse1.ville
             let name1 = ""
             if (typeof lieux.lieux[0].tags.name !== "undefined") {
                 name1 = lieux.lieux[0].tags.name
@@ -322,7 +145,7 @@ class App extends  React.Component {
                 id: '1',
                 titre: name1,
                 img: '',
-                adresse: adresse1,
+                adresse: adresseF1,
                 description: '',
                 coordonnes: [lieux.lieux[0].lat, lieux.lieux[0].lon],
                 type: this.state.themeId
@@ -338,7 +161,9 @@ class App extends  React.Component {
             coordonnes: [3, 4],    //latitude_longitude
             type: 4,
         }
-        if (lieux.lieux.lenght >= 2 && lieux.lenght >= 1){let adresse2 = await (await fetch(`http://localhost:8080/proximite-a/api/coordinates/${lieux.lieux[1].lat}_${lieux.lieux[1].lon}`)).json();
+        if (lieux.lieux.length >= 2 && typeof lieux.lieux !== "undefined") {
+            let adresse2 = await (await fetch(`http://localhost:8080/proximite-a/api/coordinates/${lieux.lieux[1].lat}_${lieux.lieux[1].lon}`)).json();
+            let adresseF2 = adresse2.rue + " " + adresse2.codepostal + " " + adresse2.ville
             let name2 = ""
             if (typeof lieux.lieux[1].tags.name !== "undefined") {
                 name2 = lieux.lieux[1].tags.name
@@ -347,7 +172,7 @@ class App extends  React.Component {
                 id: '2',
                 titre: name2,
                 img: '',
-                adresse: adresse2,
+                adresse: adresseF2,
                 description: '',
                 coordonnes: [lieux.lieux[1].lat, lieux.lieux[1].lon],
                 type: this.state.themeId
@@ -363,7 +188,9 @@ class App extends  React.Component {
             coordonnes: [5, 6],    //latitude_longitude
             type: 4,
         }
-        if (lieux.lieux.lenght >= 3 && lieux.lenght >= 1) {let adresse3 = await (await fetch(`http://localhost:8080/proximite-a/api/coordinates/${lieux.lieux[2].lat}_${lieux.lieux[2].lon}`)).json();
+        if (lieux.lieux.length >= 3 && typeof lieux.lieux !== "undefined") {
+            let adresse3 = await (await fetch(`http://localhost:8080/proximite-a/api/coordinates/${lieux.lieux[2].lat}_${lieux.lieux[2].lon}`)).json();
+            let adresseF3 = adresse3.rue + " " + adresse3.codepostal + " " + adresse3.ville
             let name3 = ""
             if (typeof lieux.lieux[0].tags.name !== "undefined") {
                 name3 = lieux.lieux[2].tags.name
@@ -372,7 +199,7 @@ class App extends  React.Component {
                 id: '3',
                 titre: name3,
                 img: '',
-                adresse: adresse3,
+                adresse: adresseF3,
                 description: '',
                 coordonnes: [lieux.lieux[2].lat, lieux.lieux[2].lon],
                 type: this.state.themeId
@@ -380,8 +207,9 @@ class App extends  React.Component {
 
         }
         let lieuSurprise = this.state.surprise
-        if (lieux.lenght > 1){let adresseSurp = await (await fetch(`http://localhost:8080/proximite-a/api/coordinates/${lieux.surprise.lat}_${lieux.surprise.lon}`)).json();
-            console.log(adresseSurp)
+        if (typeof lieux.surprise !== "undefined") {
+            let adresseSurp = await (await fetch(`http://localhost:8080/proximite-a/api/coordinates/${lieux.surprise.lat}_${lieux.surprise.lon}`)).json();
+            let adresseFS = adresseSurp.rue + " " + adresseSurp.codepostal + " " + adresseSurp.ville
             let nameSurp = ""
             if (lieux.surprise.tags.name) {
                 nameSurp = lieux.surprise.tags.name
@@ -389,7 +217,7 @@ class App extends  React.Component {
             lieuSurprise = {
                 titre: nameSurp,
                 img: '',
-                adresse: adresseSurp,
+                adresse: adresseFS,
                 description: '',
                 coordonnes: [lieux.surprise.lat, lieux.surprise.lon],
                 type: 0 //todo mettre le bon theme
@@ -397,12 +225,11 @@ class App extends  React.Component {
         }
 
         this.setState({
-            sites: [site1,site2,site3],
-            surprise:lieuSurprise
+            sites: [site1, site2, site3],
+            surprise: lieuSurprise,
+            pageId: 4
         });
-        console.log("nouveaux sites")
-        console.log([site1, site2, site3])
-        this.setState({pageId:4})
+
     }
 
     render() {
